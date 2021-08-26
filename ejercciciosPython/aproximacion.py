b6b6dbf94d83fdd08e53a5042fc019e70a93e8f5
hildebrando <gluoneros@gmail.com>
2021-08-25 23:31:29 -0500

    new exercise

--- ejercciciosPython/aproximacion.py
+++ ejercciciosPython/aproximacion.py
@@ -7,4 +7,6 @@ while   abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
     respuesta += paso
 
 if abs(respuesta**2 - objetivo) >= epsilon:
-    
+    print(f'No se encontro la raiz cuadrada {objetivo}')
+else:
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
